3dd58891cd56520590eb72a334363e4a1ef56478
Mike Cameron <mike@mikecameron.ca>
2021-05-30 22:12:21 -0600

    added sm col span

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -3,11 +3,18 @@ import { Card, CardBody, CardFooter, Col, Grid } from "tailwind-react-ui";
 import Container from "../../components/container";
 import Layout from "../../components/layout";
 
-function index() {
+
+
+function index({ subscription}) {
+  console.log(subscription)
+  
+
   return (
+    <>
     <Layout>
         <Container>
-      <div className="grid grid-cols-3 gap-4">
+      <div className="grid lg:grid-cols-4 sm:grid-cols-1 gap-4">
+        
         <Card className="block bg-white shadow-md hover:shadow-xl rounded-lg overflow-hidden mt-5">
           <CardBody>
             <div className="relative pb-48 overflow-hidden">
@@ -18,11 +25,12 @@ function index() {
               />
             </div>
 
-            <h2 className="mt-2 mb-2  font-bold">Blog Post Title</h2>
+            <h2 className="mt-2 mb-2  font-bold">test</h2>
             <div className='text-sm'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
           </CardBody>
           <CardFooter></CardFooter>
         </Card>
+        
 
         <Card className="block bg-white shadow-md hover:shadow-xl rounded-lg overflow-hidden mt-5">
           <CardBody>Inside a card</CardBody>
@@ -38,7 +46,7 @@ function index() {
               />
             </div>
 
-            <h2 class="mt-2 mb-2  font-bold">Blog Post Title</h2>
+            <h2 className="mt-2 mb-2  font-bold">Blog Post Title</h2>
             <div className='text-sm'>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</div>
           </CardBody>
           <CardFooter></CardFooter>
@@ -70,7 +78,7 @@ function index() {
         </Card>
       </div>
       </Container>
-    </Layout>
+    </Layout></>
   );
 }
 
