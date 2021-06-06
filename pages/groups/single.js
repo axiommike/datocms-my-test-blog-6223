5d635a00166123729745375f74e57e84581f850c
Mike Cameron <mike@mikecameron.ca>
2021-06-05 21:32:48 -0600

    Added a group mockup page

---
+++ pages/groups/single.js
@@ -0,0 +1,82 @@
+import React, { Component } from "react";
+import Layout from "../../components/layout";
+
+export default class index extends Component {
+  render() {
+    return (
+      <Layout>
+        <div class="bg-gray-100 pt-5">
+            <div className='max-w-screen-xl mx-auto'>
+          <div class=" w-full rounded-lg shadow-lg flex-col  mx-auto bg-white">
+            <div class="bg-white rounded-t-md">
+              <img
+                src="https://staging3.mikecameron.ca/wp-content/themes/vikinger/img/cover/default-cover.png"
+                class="rounded-t-lg object-cover w-full"
+              />
+            </div>
+            <div class="bg-white w-28 h-28 mx-auto relative -top-14  flex-row rounded-full -mb-16">
+              <div
+                class="w-24 h-24 mx-auto relative top-2  flex-row rounded-full text-white bg-contain"
+                style={{
+                  backgroundImage:
+                    "url(" +
+                    "https://staging3.mikecameron.ca/wp-content/uploads/2021/05/default-avatar.jpg" +
+                    ")",
+                }}
+              ></div>
+            </div>
+            <div class="bg-white top-5 text-left flex flex-row rounded-b-lg">
+              <div class="text-center p-3 text-gray-400 text-sm font-bold ml-8">
+                0<br />
+                Posts
+              </div>
+              <div class="text-center p-3 text-gray-400 text-sm font-bold">
+                0<br />
+                Friends
+              </div>
+              <div class="text-center p-3 text-gray-400 text-sm font-bold">
+                0<br />
+                Comments
+              </div>
+            </div>
+          </div>
+          <div class="max-w-1254 w-full rounded-lg shadow-lg flex-col bg-white mx-auto">
+            <div class="md:grid-cols-12 flex gap-1 mt-3 overflow-auto h-8">
+              <div class="w-full h-full text-center rounded-b-sm font-bold my-auto hover:border-b-4 border-blue-600">
+                Groups
+              </div>
+              <div> |</div>
+              <div class="h-8 w-full text-center font-bold">Groups</div>
+              <div> |</div>
+              <div class="h-8 w-full text-center font-bold">Groups</div>
+              <div> |</div>
+              <div class="h-8 w-full text-center font-bold">Groups</div>
+              <div> |</div>
+              <div class="h-8 w-full text-center font-bold">Groups</div>
+              <div> |</div>
+              <div class="h-8 w-full text-center font-bold">Quests</div>
+              <div> |</div>
+              <div class="h-8 w-full text-center font-bold">Forum</div>
+              <div> |</div>
+              <div class="h-8 w-full text-center font-bold">Blog</div>
+              <div> |</div>
+            </div>
+          </div>
+          {/* Layout widget columns and feed column */}
+          <div class="max-w-1254 w-full rounded-lg shadow-lg flex-col  mx-auto">
+            <div className="flex grid-cols-3 mx-auto gap-2 space-x-1 m-3">
+              <div className="bg-white w-1/6 p-4 text-center flex-col rounded-lg">
+                <div className="font-black p-3">Social Networks</div>
+                <div>No Networks Configured</div>
+                <div>Social Networks</div>
+              </div>
+              <div className="bg-white w-2/3 text-center">Feed Column</div>
+              <div className="bg-white w-1/6 text-center">Right Widget col</div>
+            </div>
+          </div>
+        </div>
+        </div>
+      </Layout>
+    );
+  }
+}
